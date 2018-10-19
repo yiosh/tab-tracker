3c9973796090e41307d32c257d48d33b045f52b7
Yiosh <iinoted@gmail.com>
2018-10-19 19:11:27 +0200

    Add controllers, routes, vuetify

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,24 +1,34 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input 
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="email"
-    >
-    <br>
-    <input 
-      type="password"
-      name="password"
-      v-model="password"
-      placeholder="password"
-      >
-    <br>
-    <button
-      @click="register"
-    >Register</button>
-  </div>
+  <v-layout align-center justify-center>
+    <v-flex xs12 sm8 md4>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <v-text-field 
+            type="email"
+            name="email"
+            v-model="email"
+            placeholder="email"
+          ></v-text-field>
+          <br>
+          <v-text-field 
+            type="password"
+            name="password"
+            v-model="password"
+            placeholder="password"
+          ></v-text-field>
+          <br>
+          <div class="error" v-html="error"></div>
+          <br>
+          <v-btn
+            @click="register"
+          >Register</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -28,16 +38,20 @@ export default {
   data() {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register() {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      });
-      console.log(response.data)
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        });
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -45,5 +59,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .error {
+    color: red;
+  }
 </style>
---
+++ server/src/models/User.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    email: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING
+    }
+  })
+  return User
+}
